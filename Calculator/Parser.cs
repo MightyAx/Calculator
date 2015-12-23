89a31984e36512ae9af19f3b54b3ad992d8e8fe8
Alexie Staffer <alexie.staffer@gmail.com>
2015-12-23 18:12:32 +0000

    Need to parse the string before we can build a function map

---
+++ Calculator/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+	public class Parser
+	{
+		public Parser ()
+		{
+		}
+
+		public List<string> Parse(string equation)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ CalculatorTests/ParserTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Calculator;
+using System;
+using System.Collections.Generic;
+
+namespace CalculatorTests
+{
+	[TestFixture ()]
+	public class ParserTests
+	{
+		[Test]
+		public void ParserCanMakeList ()
+		{
+			const string input = "4+5*2";
+			List<string> expectedResult = new List<string>(new string[] { "4", "+", "5", "*", "2"});
+
+			Parser parser = new Parser ();
+			List<string> result = parser.Parse (input);
+
+			Assert.AreEqual (expectedResult.Count, result);
+			for (int i = 0; i < expectedResult.Count; i++)
+			{
+				Assert.AreEqual (expectedResult [i], result [i]);
+			}
+		}
+	}
+}
+
